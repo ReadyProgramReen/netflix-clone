d49463c07ee84f5196277210221b583aa47dfc4b
Maureen E <maureenekeugo1@gmail.com>
2024-07-25 18:53:01 -0400

    created multiple cards on home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -30,6 +30,15 @@ const Home = () => {
             <TitleCards/>
         </div>
       </div>
+
+        <div className="more-cards">
+          <TitleCards title={"Blockbuster Movies"}/>
+          <TitleCards title={"Only on Netflix"}/>
+          <TitleCards title={"Upcoming"}/>
+          <TitleCards title={"Top Picks for You"}/>
+
+
+      </div>
     </div>
   )
 }
