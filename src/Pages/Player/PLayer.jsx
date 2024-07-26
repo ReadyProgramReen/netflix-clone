93d609f4d15e9acf087c25cb87a7f820015d041b
Maureen E <maureenekeugo1@gmail.com>
2024-07-26 10:51:54 -0400

    add functionality for dynamic movie id

--- src/Pages/Player/PLayer.jsx
+++ src/Pages/Player/PLayer.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const PLayer = () => {
+
+  const {id} = useParams();
+  const [apiData,setApiData] = useState({
+    name:"",
+    key:"",
+    published_at: "",
+    type : "",
+  });
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDZmMWJiZGFjZGZjMzJiODk1NGEzM2FlZTdmMTQ2MSIsIm5iZiI6MTcyMTk1MzA2Ni42ODQ3ODYsInN1YiI6IjY2YTJlOTgyOTk0NjYzMTFkNDQ2ODIzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qQLrsA7dyyzNkhelDgLX196AE60nNGDESfgM9sM5vUs'
+    }
+  };
+  
+  useEffect(()=>{
+     fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response =>  response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+
+  },[])
+  
+ 
+
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt="" />
-      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/iz_SJ5TpLJ0&t=3187s"
+      <iframe width="90%" height="90%" src={`https://www.youtube.com/embed/${apiData.key}`}
       title='trailer' frameBorder='0' allowFullScreen ></iframe>
 
       <div className="player-info">
-        <p>Publish Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
 
 
       </div>
