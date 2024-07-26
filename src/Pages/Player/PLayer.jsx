67fb9a4045082d4ac8a88462785fa6291e8ec1e9
Maureen E <maureenekeugo1@gmail.com>
2024-07-25 20:58:15 -0400

    Created and styled the player components

--- src/Pages/Player/PLayer.jsx
+++ src/Pages/Player/PLayer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import './player.css'
+import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const PLayer = () => {
   return (
-    <div className='player'>PLayer</div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" />
+      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/iz_SJ5TpLJ0&t=3187s"
+      title='trailer' frameBorder='0' allowFullScreen ></iframe>
+
+      <div className="player-info">
+        <p>Publish Date</p>
+        <p>Name</p>
+        <p>Type</p>
+
+
+      </div>
+    </div>
   )
 }
 
