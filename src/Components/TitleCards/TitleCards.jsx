8b719f61172580f362242ddf7319c516f9149db4
Maureen E <maureenekeugo1@gmail.com>
2024-07-25 20:32:30 -0400

    Added fetch api to titlecards component

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
@@ -6,8 +6,19 @@ import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title,category}) => {
 
+  const [apiData, setapiData] =useState([])
+
   const cardsRef =useRef();
 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDZmMWJiZGFjZGZjMzJiODk1NGEzM2FlZTdmMTQ2MSIsIm5iZiI6MTcyMTk1MzA2Ni42ODQ3ODYsInN1YiI6IjY2YTJlOTgyOTk0NjYzMTFkNDQ2ODIzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qQLrsA7dyyzNkhelDgLX196AE60nNGDESfgM9sM5vUs'
+    }
+  };
+  
+  
 const handleWheel = (event)=>{
   event.preventDefault();
   cardsRef.current.scollLeft += event.deltaY;
@@ -15,6 +26,12 @@ const handleWheel = (event)=>{
   
 }
 useEffect(()=>{
+
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => setapiData(response.results))
+    .catch(err => console.error(err));
+
   cardsRef.current.addEventListener('wheel', handleWheel);
 },[])
 
@@ -23,10 +40,10 @@ useEffect(()=>{
       <h2>{title?title: "Popular on Netflix "}</h2>
       
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((cards,index)=>{
+        {apiData.map((cards,index)=>{
             return <div className="card" key={index}>
-              <img src={cards.image} alt="" />
-              <p>{cards.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500`+cards.backdrop_path} alt="" />
+              <p>{cards.original_title}</p>
             </div>
         })}
       </div>
