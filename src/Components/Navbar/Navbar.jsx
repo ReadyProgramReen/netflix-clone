f59c5005091c4b511c97d7347070104c7123ebd4
Maureen E <maureenekeugo1@gmail.com>
2024-07-27 00:20:53 -0400

    redirected logout button on navbar to sign out

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
 import profile_img from '../../assets/profile_img.png'
 import dropdown_icon from '../../assets/caret_icon.svg'
+import { logout } from '../../firebase'
 
 
 
@@ -51,7 +52,7 @@ const Navbar = () => {
 
         {/* Only seen when we hover over profile icon */}
         <div className="dropdown">
-          <p>Sign Out of Netflix</p>
+          <p onClick={()=>{logout()}}>Sign Out of Netflix</p>
         </div>
 
 
