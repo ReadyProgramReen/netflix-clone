89c83fffd68315689d3449e1c8994ef77ca3e50e
Maureen E <maureenekeugo1@gmail.com>
2024-07-27 00:11:56 -0400

    added functionality for signup and signin to be redirected to right pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './Pages/Home/Home.jsx'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './Pages/Login/Login.jsx'
 import PLayer from './Pages/Player/PLayer.jsx'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase.js'
 
 const App = () => {
+
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, async (user)=>{
+        if(user){
+            console.log("Logged in")
+            navigate('/')
+        }else{
+          console.log("Logged Out");
+          navigate('/login')
+        }
+    })
+  },[])
+
   return (
     <div>
       <Routes>
